ed8972deba7fea013f028b84ef581029ade4ea3d
plantmanager <ponjyuusu@gmail.com>
2015-08-12 08:42:58 +0000

    8/12 17:42 migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812074317) do
+ActiveRecord::Schema.define(version: 20150812083952) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
