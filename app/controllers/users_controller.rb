b7c17c227894cd539245f9bb37e307c0386cd2eb
plantmanager <ponjyuusu@gmail.com>
2015-08-13 01:51:46 +0000

    profile 0813 10:51

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -7,6 +9,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to user_path(current_user) , notice: 'プロフィールを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -16,11 +31,15 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :profile, :email, :password,
                                  :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
